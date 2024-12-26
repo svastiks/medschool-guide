930e1c722a505691e9d5f5f549267a1a7e2809ff
svastik <svastiksharma13@gmail.com>
2024-12-25 20:52:20 -0500

    added basic structure

---
+++ src/main/java/medschoolguide/MedschoolguideApplication.java
@@ -0,0 +1,13 @@
+package medschoolguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedschoolguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedschoolguideApplication.class, args);
+	}
+
+}
