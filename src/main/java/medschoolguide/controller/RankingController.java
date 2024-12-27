d1c72b9c5072ec8db510d873caefceef27b8542f
svastik <svastiksharma13@gmail.com>
2024-12-26 23:50:23 -0500

    added controller for ranking

---
+++ src/main/java/medschoolguide/controller/RankingController.java
@@ -0,0 +1,27 @@
+package medschoolguide.controller;
+
+
+import medschoolguide.entity.Rankings;
+import medschoolguide.service.RankingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rankings")
+public class RankingController {
+    private final RankingService rankingService;
+
+    public RankingController(RankingService rankingService) {
+        this.rankingService = rankingService;
+    }
+
+    @GetMapping
+    public List<Rankings> getRankings() {
+        return rankingService.getAllRankings();
+    }
+
+
+}
