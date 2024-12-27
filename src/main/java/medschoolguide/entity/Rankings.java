697feea4b7a388d5624196082352b5a285499459
svastik <svastiksharma13@gmail.com>
2024-12-26 23:37:46 -0500

    update entity file for rankings

--- src/main/java/medschoolguide/entity/Rankings.java
+++ src/main/java/medschoolguide/entity/Rankings.java
@@ -16,11 +16,47 @@ public class Rankings {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "university name", nullable = false)
+    private Integer id;
     private String universityName;
-    @Column(name = "rankings")
-    private int ranking;
+    private Integer rank;
+    private String description;
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    @Column(name = "university name", nullable = false)
+//    private String universityName;
+//    @Column(name = "rankings")
+//    private int ranking;
 
 }
