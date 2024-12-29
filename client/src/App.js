a19f84718552d56504a15ef578be5cd1d81bd822
svastik <svastiksharma13@gmail.com>
2024-12-28 21:25:56 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Rankings from './components/Rankings/Rankings';
+import BurgerIcon from './components/BurgerIcon/BurgerIcon';
+import Sidebar from './components/Sidebar/Sidebar';
+import Home from './components/Home/Home'
+import RedditReferences from './components/RedditReferences/RedditReference';
+import Header from './components/Header/Header';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); // Toggle the sidebar open/closed
+  };
+
   return (
+    <Router>
     <div className="App">
-      <Rankings />
+      <BurgerIcon toggleSidebar={toggleSidebar} />
+      <Header/>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/rankings" element={<Rankings />}/>
+          <Route path="/reddit" element={<RedditReferences />}/>
+        </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
